810783ed6d9f34e63b45b2edca96e031d3ac490a
CR Hapuarachchi <chirantharavishka@gmail.com>
2025-01-03 00:01:28 +0530

    Adding GCS bucket demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,25 @@ terraform {
       source = "hashicorp/google"
       version = "6.14.1"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
   }
 }
 
 provider "google" {
   project     = "testmap-417607"
   region      = "us-east1"
+}
+
+data "google_client_config" "default"{
+} 
+
+provider "docker" {
+  registry_auth {
+    address = "us-east1-docker.pkg.dev"
+    username = "oauth2accesstoken"
+    password = data.google_client_config.default.access_token
+  }
 }
---
+++ terraform/service.tf
@@ -0,0 +1,19 @@
+resource "docker_image" "chiradev_demo" {
+  name = "us-east1-docker.pkg.dev/testmap-417607/${google_artifact_registry_repository.registry.name}/chiradev-demo-tf"
+  build {
+    context = "../src/"
+  }
+} 
+
+resource "google_artifact_registry_repository" "registry" {
+  location      = "us-east1"
+  repository_id = "chiradev-demo-repo"
+  format        = "DOCKER"
+}
+
+resource "docker_registry_image" "chiradev_image" {
+  name          = docker_image.chiradev_demo.name
+  keep_remotely = true
+  depends_on = [ docker_image.chiradev_demo,google_artifact_registry_repository.registry ]
+}
+
